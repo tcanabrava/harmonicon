3295cc7c6c7856fa54e83d2fc482eeca73b13839
Tomaz Canabrava <tcanabrava@kde.org>
2026-06-10 15:11:51 +0200

    Add Font for the game

--- src/gameplay/mod.rs
+++ src/gameplay/mod.rs
@@ -9,6 +9,7 @@ use crate::{
         chart::{Action, BendingProfile, HarpChart, Harmonica},
         SongManifest,
     },
+    audio_system::midi_functions::{note_to_midi, midi_to_note},
 };
 
 pub struct GameplayPlugin;
@@ -156,33 +157,6 @@ fn harp_display(chart: &HarpChart) -> String {
             format!("Chromatic \u{00B7} {} holes \u{00B7} {} position", holes, pos)
         }
     }
-}
-
-// Convert a note string like "G4", "C#5", "Bb3" to a MIDI number.
-fn note_to_midi(note: &str) -> Option<i32> {
-    const NAMES: [&str; 12] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-    let (pitch, oct_str) = if note.len() >= 3
-        && (note.as_bytes().get(1) == Some(&b'#') || note.as_bytes().get(1) == Some(&b'b'))
-    {
-        (&note[..2], &note[2..])
-    } else {
-        (&note[..1], &note[1..])
-    };
-    // Normalise flats to enharmonic sharps.
-    let pitch = match pitch {
-        "Db" => "C#", "Eb" => "D#", "Fb" => "E", "Gb" => "F#",
-        "Ab" => "G#", "Bb" => "A#", "Cb" => "B", p => p,
-    };
-    let semitone = NAMES.iter().position(|&n| n == pitch)? as i32;
-    let octave: i32 = oct_str.parse().ok()?;
-    Some((octave + 1) * 12 + semitone)
-}
-
-fn midi_to_note(midi: i32) -> String {
-    const NAMES: [&str; 12] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-    let semitone = ((midi % 12) + 12) % 12;
-    let octave = midi / 12 - 1;
-    format!("{}{}", NAMES[semitone as usize], octave)
 }
 
 // Build the complete set of notes this harmonica can physically produce,
